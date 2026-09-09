5d35417ee35a5872381205771a9656c050538db0
Aiden-Ormond <158118656+Aiden-Ormond@users.noreply.github.com>
2026-09-09 22:02:58 +0800

    Update volume_controller.py

--- app/volume_controller.py
+++ app/volume_controller.py
@@ -1,13 +1,15 @@
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import time, threading
+from pycaw.pycaw import IAudioEndpointVolume
+from pycaw.api.mmdeviceapi import IMMDeviceEnumerator, EDataFlow, ERole
 
 class VolumeController:
     def __init__(self):
-        devices = AudioUtilities.GetSpeakers()
-        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-        self.volume = cast(interface, POINTER(IAudioEndpointVolume))
+        # 使用底层 API 避免 AudioDevice 包装类没有 Activate 方法的问题
+        enumerator = IMMDeviceEnumerator()
+        # 获取默认音频渲染设备（扬声器）
+        speakers = enumerator.GetDefaultAudioEndpoint(EDataFlow.eRender.value, ERole.eMultimedia.value)
+        self.volume = cast(speakers.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None), POINTER(IAudioEndpointVolume))
 
     def get_current_volume(self):
         return self.volume.GetMasterVolumeLevelScalar()
@@ -23,6 +25,8 @@ class VolumeController:
             self.set_volume(target_scalar)
             return
         increment = (target_scalar - start) / steps
+        
+        import time, threading
         def _increase():
             current = start
             for i in range(steps):
