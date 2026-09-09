b12f5ca478321f5b6a4ef4c75ad6ce9a28c7bbb7
Aiden-Ormond <158118656+Aiden-Ormond@users.noreply.github.com>
2026-09-09 22:13:59 +0800

    Update player.py

--- app/player.py
+++ app/player.py
@@ -6,7 +6,6 @@ class Player:
         self.vol_ctrl = VolumeController()
 
     def play_netease(self, playlist_id_or_song_id, type='playlist'):
-        """通过 URL Scheme 启动网易云并播放指定内容"""
         if type == 'playlist':
             uri = f"orpheus://playlist/{playlist_id_or_song_id}"
         else:
@@ -14,12 +13,10 @@ class Player:
         subprocess.Popen(["start", uri], shell=True)
 
     def alarm(self, target_id, target_type='playlist', volume=0.5, gradual_duration=30):
-        """执行闹钟：先渐入音量，再播放网易云"""
         self.vol_ctrl.gradual_increase(volume, gradual_duration)
         time.sleep(gradual_duration)
         self.play_netease(target_id, target_type)
 
     def shutdown_after(self, minutes):
-        """定时休眠"""
         seconds = int(minutes * 60)
         os.system(f'shutdown /h /t {seconds}')
